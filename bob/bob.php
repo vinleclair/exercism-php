ce894f8afaaa06aa894d669237fe62880167b09a
Vincent Leclair <vinleclair@gmail.com>
2019-04-29 00:54:34 -0400

    Add lackadaisical teenager

---
+++ bob/bob.php
@@ -0,0 +1,42 @@
+<?php
+
+class Bob
+{
+    public function respondTo(string $statement) : string 
+    {
+        if (Statement::isQuestion($statement) && Statement::isYelling($statement)) {
+            return "Calm down, I know what I'm doing!";
+        } elseif (Statement::isQuestion($statement)) {
+            return "Sure.";
+        } elseif (Statement::isYelling($statement)) {
+            return "Whoa, chill out!";
+        } elseif (Statement::isSilence($statement)) {
+            return "Fine. Be that way!";
+        } else {
+            return "Whatever.";
+        }
+    }
+}
+
+class Statement
+{
+    public static function isSilence(string $statement) : bool 
+    {
+        return trim($statement) === "";
+    }
+
+    public static function isYelling(string $statement) : bool
+    {
+        return self::isWords($statement) && strtoupper($statement) === $statement;
+    }
+
+    private static function isWords(string $statement) : bool
+    {
+        return strtoupper($statement) != strtolower($statement);
+    }
+
+    public static function isQuestion(string $statement) : bool
+    {
+        return substr(rtrim($statement), -1) === "?";
+    }
+}
