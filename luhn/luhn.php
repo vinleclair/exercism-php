df1196ae42da54823b76c73d5c5264a86a24858b
Vincent Leclair <vinleclair@gmail.com>
2019-05-06 15:49:43 -0400

    Refactor into constant array

--- luhn/luhn.php
+++ luhn/luhn.php
@@ -1,5 +1,18 @@
 <?php
 
+define("LUHN", [
+    0 => 0,
+    1 => 2,
+    2 => 4,
+    3 => 6,
+    4 => 8,
+    5 => 1,
+    6 => 3,
+    7 => 5,
+    8 => 7,
+    9 => 9
+]);
+
 /**
  * Given a number returns true if valid per the Luhn formula, else false
  * @param string $number to validate
@@ -10,15 +23,15 @@ function isValid(string $number) : bool
     $number = str_replace(" ", "", $number);
 
     // Regex check if number contains only 2 or more digits, else return false
-    if (preg_match('/^[\d]{2,}$/', $number) != 1) { return false; };
+    if (preg_match('/^[\d]{2,}$/', $number) !== 1) { return false; };
 
     $digits = array_reverse(str_split($number));
+    $digitsCount = count($digits);
 
-    for ($i = 1; $i < count($digits); $i += 2) {
-        $result = $digits[$i] * 2;
-        $digits[$i] = ($result > 9) ? $result - 9 : $result; 
+    for ($i = 1; $i < $digitsCount; $i += 2) {
+        $digits[$i] = LUHN[$digits[$i]]; 
     }
 
-    return array_sum($digits) % 10 == 0;
+    return array_sum($digits) % 10 === 0;
 }
 
