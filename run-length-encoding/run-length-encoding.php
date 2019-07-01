84d7e3d21d6c2af75efe9eb794e764c868a8c4ce
Vincent Leclair <vinleclair@gmail.com>
2019-07-01 15:44:56 -0400

    Add run length encoding & decoding

---
+++ run-length-encoding/run-length-encoding.php
@@ -0,0 +1,20 @@
+<?php
+
+function encode($input)
+{
+    return preg_replace_callback('/(.)\1*/', 
+        function($matches) {
+            return strlen($matches[0]) == 1 ? $matches[1] : strlen($matches[0]) . $matches[1];
+        },
+        $input);
+}
+
+function decode($input)
+{
+    return preg_replace_callback('/(\d+)(.)/', 
+        function($matches) { 
+            return str_repeat($matches[2], $matches[1]); 
+        }, 
+        $input);
+}
+
